259f12071e2cb52f8cbee65dafc1d2d461d7dbe1
Anders K. Andersen <aka@myself.com>
2020-03-10 19:08:40 +0100

    added test of date format + printing transactions

--- catcher-app/src/main/java/dk/bec/catcher/app/step01makeItWork/AmlCatcher01App.java
+++ catcher-app/src/main/java/dk/bec/catcher/app/step01makeItWork/AmlCatcher01App.java
@@ -92,6 +92,15 @@ public class AmlCatcher01App {
             this.postingText = postingText;
             this.amount = amount;
         }
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("{");
+            sb.append("date=").append(date);
+            sb.append(", postingText='").append(postingText).append('\'');
+            sb.append(", amount=").append(amount);
+            sb.append('}');
+            return sb.toString();
+        }
     }
 
 }
--- catcher-app/src/test/java/dk/bec/catcher/app/step01makeItWork/AmlCatcher01AppTest.java
+++ catcher-app/src/test/java/dk/bec/catcher/app/step01makeItWork/AmlCatcher01AppTest.java
@@ -10,6 +10,10 @@ import java.util.List;
 public class AmlCatcher01AppTest {
 
     @Test
+    public void testUnderstandingDateFormat() {
+        Assert.assertEquals("Check that the date format is understood",LocalDate.of(2020,4,1),LocalDate.parse("2020-04-01"));
+    }
+    @Test
     public void selectPostings() {
         AmlCatcher01App.Repository repository= new AmlCatcher01App.Repository();
         List<AmlCatcher01App.Posting> candidates=repository.selectPostings(LocalDate.parse("2020-04-01"));
@@ -22,6 +26,9 @@ public class AmlCatcher01AppTest {
         List<AmlCatcher01App.Posting> candidates = catcher.checkFraudAtDay(LocalDate.parse("2020-04-01"));
 
         Assert.assertEquals(2,candidates.size());
+        for(AmlCatcher01App.Posting candidate:candidates){
+            System.out.println(candidate);
+        }
     }
     @Test
     public void selectPostingsAtDayWithNoPostings() {
