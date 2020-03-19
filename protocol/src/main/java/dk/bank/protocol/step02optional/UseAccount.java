1bf9972bd5b966cbeaaf08ce43343cde95aa1752
Anders K. Andersen <aka@myself.com>
2020-03-19 16:17:18 +0100

    doc

--- protocol/src/main/java/dk/bank/protocol/step02optional/UseAccount.java
+++ protocol/src/main/java/dk/bank/protocol/step02optional/UseAccount.java
@@ -2,13 +2,22 @@ package dk.bank.protocol.step02optional;
 
 import java.util.Optional;
 
-
+/**
+ * Dealing with methods that can return nothing or something
+ * See: https://www.baeldung.com/java-optional
+ *
+ */
 public class UseAccount {
 
     /**
      * Sample of return no account or an account
      */
     interface Protocol {
+        /**
+         * Getting the customers main account - not all customers got a main account
+         * @param customer customer
+         * @return result - Optional is empty when no account.
+         */
         Optional<Account> getCustomerMainAccount(String customer);
 
     }
